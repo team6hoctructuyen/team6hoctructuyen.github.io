d762d687da08fd2c290749bbf676c0faa19c10ff
NguyenHaTienLoc <hoatranhoameo2@gmail.com>
2024-04-10 04:01:57 +0700

    final

--- script1.js
+++
@@ -1,8 +0,0 @@
-function myMenuFunction() {
-    var i = document.getElementById("navMenu");
-    if(i.className === "nav-menu") {
-        i.className += " responsive";
-    } else {
-        i.className = "nav-menu";
-    }
-   }
